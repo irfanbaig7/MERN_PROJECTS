fdf0b179cc36819fd89e29edfd1846d8031e1705
irfanbaig7 <ibaig9356@gmail.com>
2025-08-13 08:21:43 +0530

    project setup, making server side connetion, making routes, controller, and more

---
+++ VideoCallingAndChat/backend/src/controller/auth.controller.js
@@ -0,0 +1,11 @@
+// import { User } from "../models/user.model";
+
+export const signup =  async (req, res) => {
+    res.send("Signup User")
+}
+export const login =  async (req, res) => {
+    res.send("Login User")
+}
+export const logout =  async (req, res) => {
+    res.send("Logout User")
+}
---
+++ VideoCallingAndChat/backend/src/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import express from "express"
+import { login, logout, signup } from "../controller/auth.controller.js"
+const authRoutes = express.Router()
+
+authRoutes.post("/signup", signup)
+authRoutes.post("/login", login)
+authRoutes.post("/logout", logout)
+
+export default authRoutes   
