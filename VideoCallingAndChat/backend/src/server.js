720f472b55ffb30b172c6da9b3ade36b300f5056
irfanbaig7 <ibaig9356@gmail.com>
2025-08-26 20:53:52 +0530

    using cors we can allow to frontend orgin to cummunicate with backend with cookies parsing also allow using into creadentials

--- VideoCallingAndChat/backend/src/server.js
+++ VideoCallingAndChat/backend/src/server.js
@@ -5,7 +5,15 @@ import cookieParser from "cookie-parser"
 import userRoutes from "./routes/users.routes.js";
 import chatRoutes from "./routes/chat.routes.js";
 
+import cors from "cors"
+
 const app = express()
+
+// cummunicate with the fronend using cors
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true // allow to frontend send the cookies
+}))
 
 app.use(express.json())
 app.use(cookieParser()) // now u can accesable to use cookies inside ur request
